267ce94be278ffd8f2f8c20cceff16a42ab95d73
timon <timon2011@gmail.com>
2026-05-27 00:17:46 +0530

    init issue changes made

--- bootstrap/provider.tf
+++ bootstrap/provider.tf
@@ -2,13 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=4.1.0"
+      version = "~> 4.0"        
     }
   }
 }
 
 provider "azurerm" {
-  subscription_id = "0eb7429e-1f06-4f79-b704-47c395f9b3c0" 
-  resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  features {}
+  features {}                   
 }
